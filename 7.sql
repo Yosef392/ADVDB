8bdb2f81dc00aadb7705f07dcaee93b1fd2436fb
Yousefmedhat <Yousefmedhat135@gmail.com>
2025-12-23 10:41:54 +0200

    edited 7.sql

--- 7.sql
+++ 7.sql
@@ -3,13 +3,16 @@ as
 
     CURSOR admission_cursor IS
     select count(id) from MANAGER.AuditTrail where operation = 'ADMIT_PATIENT';
+    total_admissions number;
 
     CURSOR discharge_cursor IS
     select count(id) from MANAGER.AuditTrail where operation = 'DISCHARGE';
+    total_discharges number;
 
-    total_admissions MANAGER.AuditTrail.id%TYPE;
+    CURSOR top_doctors_cursor IS
+    select d.id, count(d.id) as treatments_count where Manager.Doctors d join Manager.AuditTrail t on d.id = t.id order by count(d.id);
+    top_doctors top_doctors_cursor%ROWTYPE;
 
-    total_discharges MANAGER.AuditTrail.id%TYPE;
     
 BEGIN
 
@@ -22,6 +25,15 @@ BEGIN
     FETCH discharge_cursor into total_discharges;
     DBMS_OUTPUT.PUT_LINE ('Total Discharges = ' || total_discharges);
     close discharge_cursor;
+
+    OPEN discharge_cursor;
+    loop
+        FETCH top_doctors_cursor into top_doctors
+        EXIT WHEN top_doctors_cursor %ROWCOUNT > 3;
+        DBMS_OUTPUT.PUT_LINE ('Top 3 Doctors: \n');
+        DBMS_OUTPUT.PUT_LINE (top_doctors.id || ' ' || top_doctors.treatments_count || ' Treatments');
+    end loop;
+    close top_doctors_cursor;
 end;
 /
 
