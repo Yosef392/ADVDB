eb9867ba16815e8c185f73bcfa65f5a5b0c4181b
Yousefmedhat <Yousefmedhat135@gmail.com>
2025-12-23 11:43:51 +0200

    edited 7.sql

--- 7.sql
+++ 7.sql
@@ -9,8 +9,18 @@ as
     select count(id) from MANAGER.AuditTrail where operation = 'DISCHARGE';
     total_discharges number;
 
+    CURSOR pateint_stay_cursor IS
+    select round(AVG(d.action_date - a.action_date)) as avg_stay_days
+    from Manager.AuditTrail a
+    JOIN MANAGER.AuditTrail d 
+    ON REGEXP_SUBSTR(a.new_data, 'Patient ID: ([0-9]+)...') = REGEXP_SUBSTR(d.new_data, 'Patient ID: ([0-9]+)...')
+    where a.operation <> d.operation
+    and d.action_date > a.action_date;
+    avg_stay_days number;
+
     CURSOR top_doctors_cursor IS
-    select d.name as id, count(d.id) as treatments_count from Manager.Doctors d join Manager.treatments t on d.id = t.doctor_id GROUP BY d.id,d.name order by count(d.id) ;
+    select d.name as id, count(d.id) as treatments_count from Manager.Doctors d 
+    join Manager.treatments t on d.id = t.doctor_id GROUP BY d.id,d.name order by count(d.id) ;
     top_doctors top_doctors_cursor%ROWTYPE;
 
     
@@ -34,6 +44,12 @@ BEGIN
         DBMS_OUTPUT.PUT_LINE (top_doctors.id || ' ' || top_doctors.treatments_count || ' Treatments');
     end loop;
     close top_doctors_cursor;
+
+    OPEN pateint_stay_cursor;
+    FETCH pateint_stay_cursor into avg_stay_days;
+    EXIT WHEN top_doctors_cursor%NOTFOUND;
+    DBMS_OUTPUT.PUT_LINE ('Average patient stay duration (in days) '|| avg_stay_days);
+    close pateint_stay_cursor;
 end;
 /
 EXEC generate_performace_report;
