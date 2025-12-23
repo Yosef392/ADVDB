768c316f5ce06a266e9d5537333303f8a6d6db2d
Yousefmedhat <Yousefmedhat135@gmail.com>
2025-12-23 06:36:31 +0200

    fixed task 7

--- 7.sql
+++ 7.sql
@@ -17,39 +17,7 @@ BEGIN
 
     OPEN admission_cursor;
     FETCH admission_cursor into admission;
-    select count(id) into total_admissions;create or replace procedure generate_performace_report()
-as
-
-    CURSOR admission_cursor IS
-    select id from MANAGER.AuditTrail where operation = 'ADMIT_PATIENT';
-
-    CURSOR discharge_cursor IS
-    select id from MANAGER.AuditTrail where operation = 'DISCHARGE';
-
-    admissions MANAGER.AuditTrail.id%TYPE;
-    total_admissions number;
-
-    discharges MANAGER.AuditTrail.id%TYPE;
-    total_discharges number;
-    
-BEGIN
-
-    OPEN admission_cursor;
-    FETCH admission_cursor into admission;
     select count(id) into total_admissions;
-    DBMS_OUTPUT.PUT_LINE ('Total Admissions = ' || total_admissions);
-    close admission_cursor;
-
-    OPEN discharge_cursor;
-    FETCH discharge_cursor into discharge;
-    select count(id) into total_discharges;
-    DBMS_OUTPUT.PUT_LINE ('Total Discharges = ' || total_discharges);
-    close discharge_cursor;
-end;
-/
-
-
-
     DBMS_OUTPUT.PUT_LINE ('Total Admissions = ' || total_admissions);
     close admission_cursor;
 
